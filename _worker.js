db4d55a8a2c890ec323095a30d85a0b659871e41
zula-ir-com-top-gama-top-xiaomi-top <Saysadra@gmail.com>
2024-07-08 23:18:08 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '36159d40-fd67-4f4c-8eed-810a8f7db549';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
